e52787370f68d521df85399474077ef31e1c41a7
Dmitriy12441 <124509035+Dmitriy12441@users.noreply.github.com>
2023-04-02 22:25:26 +0300

    Update Program.cs
    Написан код для решения задачи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
+Console.Write("Введите массив данных через ',': ");
+string strLine = Console.ReadLine();
+string[] Array1 = strLine.Split(",");
+string[] Array2 = new string[0];
 
+for (int i = 0; i < Array1.Length; i++)
+{
+    if (Array1[i].Length <= 3)
+    {
+        Array.Resize(ref Array2, Array2.Length + 1);
+        Array2[Array2.Length-1] = Array1[i];
+    }
+
+}
+Console.WriteLine($"[{string.Join(",", Array1)}]=>[{string.Join(",", Array2)}]");
